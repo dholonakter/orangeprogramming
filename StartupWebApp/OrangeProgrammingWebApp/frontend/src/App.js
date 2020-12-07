19dfef71e8672d216687d1758cbc78f8924e2ea8
Dholon Akter <dholonakter721@gmail.com>
2020-12-07 14:50:09 +0100

    research document and forntend part updated

--- StartupWebApp/OrangeProgrammingWebApp/frontend/src/App.js
+++ StartupWebApp/OrangeProgrammingWebApp/frontend/src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+
 import React, { Component } from 'react';
+import Toolbar from './components/Toolbar/Toolbar';
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello World!</h1>
+        <Toolbar/>
+       
       </div>
     );
   }
