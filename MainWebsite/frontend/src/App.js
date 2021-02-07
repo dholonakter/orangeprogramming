a5b2e105ce7df53a82b0ebe10a77bf957cf3b5c5
chistyhimel <fc_himel@yahoo.com>
2021-02-07 10:18:42 +0600

    project setup

--- MainWebsite/frontend/src/App.js
+++ MainWebsite/frontend/src/App.js
@@ -1,10 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./pages/Home/Home.page";
 
 function App() {
   return (
     <div className="App">
-     Hello world
+      <Router>
+        <Switch>
+          <Route path="/home">
+            <Home />
+          </Route>
+          
+          <Route exact path="/">
+            <Home />
+          </Route>
+
+          <Route exact path="*">
+            {/* Page Not Found Here */}
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
