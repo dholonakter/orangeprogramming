c506bef4e06f82d04d20156078191aa83a0abd11
chistyhimel <fc_himel@yahoo.com>
2021-06-15 23:56:10 +0600

    methodology-accordion-added

--- MainWebsite/frontend/src/components/Methodology/Methodology.component.jsx
+++ MainWebsite/frontend/src/components/Methodology/Methodology.component.jsx
@@ -1,5 +1,8 @@
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Accordion from "@material-ui/core/Accordion";
+import AccordionDetails from "@material-ui/core/AccordionDetails";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
 import React, { useState } from "react";
 import { Row, Col } from "react-grid-system";
 import { Container } from "../../utils/container";
@@ -11,11 +14,17 @@ import {
 } from "./Methodology.style";
 import { HeaderText } from "../../utils/text";
 import { SmallBorderBottom } from "../../utils/constant";
+import useWindowDimensions from "../../utils/windowDimentions";
+import Typography from "@material-ui/core/Typography";
 
-function Methodology({ data, header}) {
+function Methodology({ data, header }) {
   const [selectedMethodology, setSelectedMethodology] = useState(data[0]);
+  const [expanded, setExpanded] = React.useState("panel1");
+  const { height, width } = useWindowDimensions();
 
-  console.log(selectedMethodology);
+  const handleChange = (panel) => (event, newExpanded) => {
+    setExpanded(newExpanded ? panel : false);
+  };
 
   return (
     <>
@@ -23,45 +32,66 @@ function Methodology({ data, header}) {
         <MethodologyWrapper>
           <HeaderText>{header}</HeaderText>
           <SmallBorderBottom />
-          <Row>
-            <Col md={6}>
-              {data.map((item) => (
-                <MethodologyItems key={item.id}>
-                  <div
-                    className={
-                      item.id === selectedMethodology.id
-                        ? "methodology__active"
-                        : "methodology__inactive"
-                    }
-                  >
-                    <p
-                      onClick={() => {
-                        setSelectedMethodology(item);
-                      }}
+
+          {width >= 768 ? (
+            <Row>
+              <Col md={6}>
+                {data.map((item) => (
+                  <MethodologyItems key={item.id}>
+                    <div
+                      className={
+                        item.id === selectedMethodology.id
+                          ? "methodology__active"
+                          : "methodology__inactive"
+                      }
                     >
-                      {item.title}
-                    </p>
-                    {item.id === selectedMethodology.id ? (
-                      <FontAwesomeIcon
-                        icon={faArrowRight}
-                        className="methodology__icon"
-                      />
-                    ) : (
-                      ""
-                    )}
-                  </div>
-                </MethodologyItems>
-              ))}
-            </Col>
-            <Col md={6}>
-              <Fade bottom cascade>
-                <MethodologyDescWrap>
-                  <h3>{selectedMethodology.title}</h3>
-                  <h5>{selectedMethodology.description}</h5>
-                </MethodologyDescWrap>
-              </Fade>
-            </Col>
-          </Row>{" "}
+                      <p
+                        onClick={() => {
+                          setSelectedMethodology(item);
+                        }}
+                      >
+                        {item.title}
+                      </p>
+                      {item.id === selectedMethodology.id ? (
+                        <FontAwesomeIcon
+                          icon={faArrowRight}
+                          className="methodology__icon"
+                        />
+                      ) : (
+                        ""
+                      )}
+                    </div>
+                  </MethodologyItems>
+                ))}
+              </Col>
+              <Col md={6}>
+                <Fade bottom cascade>
+                  <MethodologyDescWrap>
+                    <h3>{selectedMethodology.title}</h3>
+                    <h5>{selectedMethodology.description}</h5>
+                  </MethodologyDescWrap>
+                </Fade>
+              </Col>
+            </Row>
+          ) : (
+            data.map((item, idx) => (
+              <Accordion
+                square
+                expanded={expanded === `panel${idx + 1}`}
+                onChange={handleChange(`panel${idx + 1}`)}
+              >
+                <AccordionSummary
+                  aria-controls="panel1d-content"
+                  id="panel1d-header"
+                >
+                  <Typography variant="h6">{item.title}</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography>{selectedMethodology.description}</Typography>
+                </AccordionDetails>
+              </Accordion>
+            ))
+          )}
         </MethodologyWrapper>
       </Container>
     </>
